9c26d17f00cfab55b29eda563443fceb5781dc39
roastduck <beantang.tang@gmail.com>
2016-03-11 21:52:58 +0800

    week 2 revision 1

--- Week2/2015011308_唐适之/Problem4/main.cpp
+++ Week2/2015011308_唐适之/Problem4/main.cpp
@@ -3,10 +3,21 @@
 
 int main()
 {
+    /*
+     * original code:
     Hen hen;
     hen.display();
     hen.nest.display();
     hen.nest.egg.display();
+     */
+    
+    // zyn says it requires to be as below:
+    Hen hen;
+    Hen::Nest nest;
+    Egg egg;
+    hen.display();
+    nest.display();
+    egg.display();
     return 0;
 }
 
